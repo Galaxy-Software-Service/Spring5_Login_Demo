969f71d805cf25eac378462e8276e5099b036a11
Obinna A. Kalu <obkalu@gmail.com>
2018-07-20 20:51:01 -0500

    Initial working cut

---
+++ src/main/java/com/obinna/spring/demos/springsecuritydemo/SpringsecurityDemoApplication.java
@@ -0,0 +1,12 @@
+package com.obinna.spring.demos.springsecuritydemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringsecurityDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringsecurityDemoApplication.class, args);
+	}
+}
---
+++ src/test/java/com/obinna/spring/demos/springsecuritydemo/SpringsecurityDemoApplicationTests.java
@@ -0,0 +1,16 @@
+package com.obinna.spring.demos.springsecuritydemo;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringsecurityDemoApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
